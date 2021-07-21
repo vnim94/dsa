e1431548a19150e17cf5e25d731c69ccdfeb8f88
phunky-panda94 <victor.nim94@outlook.com>
2021-07-21 19:34:34 +1000

    added recursive method

--- binarySearch.py
+++ binarySearch.py
@@ -1,5 +1,4 @@
-# iterative
-def binarySearch(array, x):
+def iterativeBinarySearch(array, target):
 
 	start = 0
 	end = len(array) - 1
@@ -15,9 +14,21 @@ def binarySearch(array, x):
 		else:
 			end = mid - 1
 
+def recursiveBinarySearch(array, target, start, end):
+
+	mid = (start + end) // 2
+
+	if array[mid] == target:
+		return mid
+	elif array[mid] < target:
+		return recursiveBinarySearch(array, target, mid + 1, end)
+	else:
+		return recursiveBinarySearch(array, target, start, mid - 1)
+	
+
 array = [2, 10, 15, 20, 25]
 target = 25
 
-print('Target is at index %d' % binarySearch(array, target)) 
+print('Target is at index %d' % recursiveBinarySearch(array, target, 0, 4)) 
 
 	
