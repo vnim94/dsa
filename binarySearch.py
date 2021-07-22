c838d2936cf3876ba31d9f346339060b8e4cc586
phunky-panda94 <victor.nim94@outlook.com>
2021-07-22 10:27:38 +1000

    added outlier case for if target not found

--- binarySearch.py
+++ binarySearch.py
@@ -1,25 +1,31 @@
 def iterativeBinarySearch(array, target):
 
+	length = len(array)
 	start = 0
-	end = len(array) - 1
+	end = length - 1
 
 	while start <= end:
 		
 		mid = (start + end) // 2
-		
+	
 		if array[mid] == target:
-			return mid
+			return 'target found at index ' + str(mid)
 		elif array[mid] < target:
 			start = mid + 1
 		else:
 			end = mid - 1
 
+	return 'target not found'
+
 def recursiveBinarySearch(array, target, start, end):
+
+	if start > end:
+		return 'target not found'
 
 	mid = (start + end) // 2
 
 	if array[mid] == target:
-		return mid
+		return 'target found at index ' + str(mid)
 	elif array[mid] < target:
 		return recursiveBinarySearch(array, target, mid + 1, end)
 	else:
@@ -27,8 +33,6 @@ def recursiveBinarySearch(array, target, start, end):
 	
 
 array = [2, 10, 15, 20, 25]
-target = 25
+target = 50
 
-print('Target is at index %d' % recursiveBinarySearch(array, target, 0, 4)) 
-
-	
+print(recursiveBinarySearch(array, target, 0, 4))
