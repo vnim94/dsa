3f1af3398991d41c747e4ee5e5d916d0423f46e1
phunky-panda94 <victor.nim94@outlook.com>
2021-12-08 23:16:04 +1100

    implemented insert and search method of BST

--- bst/binary_search_tree.py
+++ bst/binary_search_tree.py
@@ -1,20 +1,33 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
+class BinarySearchTree:
+    def __init__(self, data=None):
+        self.root = data
         self.left = None
         self.right = None
 
-class BinarySearchTree:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, root, value):
-        # if tree is empty, insert at root
-
-        # if value > root.data, insert in right-subtree
-
-        # else, insert in left sub-tree
- 
-
-    def search(self):
-        None
+    def insert(self, value):
+        if self.root == None:
+            self.root = value
+        elif value > self.root:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+        else:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        
+    def search(self, value):
+        if self.root == value:
+            return True
+        if value > self.root:
+            if self.right != None:
+                return self.right.search(value)
+            else:
+                return False
+        else:
+            if self.left != None:
+                return self.left.search(value)
+            else:
+                return False
--- bst/binary_search_tree_test.py
+++ bst/binary_search_tree_test.py
@@ -1,7 +1,7 @@
 import unittest
-from binary_search_tree import Node, BinarySearchTree
+from binary_search_tree import BinarySearchTree
 
-class BinarySearchTreeTest(unittest.TestCase):
+class test_insert_method(unittest.TestCase):
     
     def test_initialise(self):
         bst = BinarySearchTree()
@@ -10,57 +10,83 @@ class BinarySearchTreeTest(unittest.TestCase):
 
     def test_insert(self):
         bst = BinarySearchTree()
-        bst.insert(0, 1)
+        bst.insert(1)
 
-        self.assertEqual(bst.root.data, 1)
-        
-    @unittest.skip
+        self.assertEqual(bst.root, 1)
+
     def test_insert_value_greater_than_root(self):
         bst = BinarySearchTree()
         bst.insert(1)
         bst.insert(2)
 
-        self.assertEqual(bst.root.data, 1)
-        self.assertEqual(bst.root.right.data, 2)
+        self.assertEqual(bst.root, 1)
+        self.assertEqual(bst.right.root, 2)
 
-    @unittest.skip
     def test_insert_value_less_than_root(self):
         bst = BinarySearchTree()
         bst.insert(2)
         bst.insert(1)
 
-        self.assertEqual(bst.root.data, 2)
-        self.assertEqual(bst.root.left.data, 1)
+        self.assertEqual(bst.root, 2)
+        self.assertEqual(bst.left.root, 1)
 
-    @unittest.skip
     def test_insert_multiple_values(self):
         bst = BinarySearchTree()
         bst.insert(2)
         bst.insert(1)
         bst.insert(3)
 
-        self.assertEqual(bst.root.data, 2)
-        self.assertEqual(bst.root.left.data, 1)
-        self.assertEqual(bst.root.right.data, 3)
+        self.assertEqual(bst.root, 2)
+        self.assertEqual(bst.left.root, 1)
+        self.assertEqual(bst.right.root, 3)
 
-    @unittest.skip
     def test_insert_multiple_values_greater_than_root(self):
         bst = BinarySearchTree()
         bst.insert(1)
         bst.insert(2)
         bst.insert(3)
 
-        self.assertEqual(bst.root.data, 1)
-        self.assertEqual(bst.root.right.data, 2)
-        self.assertEqual(bst.root.right.data, 3)
+        self.assertEqual(bst.root, 1)
+        self.assertEqual(bst.right.root, 2)
+        self.assertEqual(bst.right.right.root, 3)
 
-    @unittest.skip
     def test_insert_multiple_values_less_than_root(self):
         bst = BinarySearchTree()
         bst.insert(2)
         bst.insert(1)
         bst.insert(0)
 
-        self.assertEqual(bst.root.data, 2)
-        self.assertEqual(bst.root.left.data, 1)
-        self.assertEqual(bst.root.left.left.data,0)
+        self.assertEqual(bst.root, 2)
+        self.assertEqual(bst.left.root, 1)
+        self.assertEqual(bst.left.left.root,0)
+
+class test_search_method(unittest.TestCase):
+
+    def test_search_for_value_at_root(self):
+        bst = BinarySearchTree()
+        bst.insert(1)
+
+        self.assertTrue(bst.search(1))
+
+    def test_search_for_value_in_right_sub_tree(self):
+        bst = BinarySearchTree()
+        bst.insert(1)
+        bst.insert(2)
+
+        self.assertTrue(bst.search(2))
+
+    def test_search_for_value_in_left_sub_tree(self):
+        bst = BinarySearchTree()
+        bst.insert(2)
+        bst.insert(1)
+
+        self.assertTrue(bst.search(1))
+
+@unittest.skip
+class test_to_string_method(unittest.TestCase):
+
+    def test_print_root_value(self):
+        bst = BinarySearchTree()
+        bst.insert(1)
+
+        self.assertEqual(bst.__str__(), 1)
